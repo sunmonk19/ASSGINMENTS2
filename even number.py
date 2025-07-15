0275f97808b80e5ab37ab65ad6d04d916327e2de
SUNMONK <sunajsu7854@gmail.com>
2025-07-15 16:31:22 +0530

    COMMIT

---
+++ even number.py
@@ -0,0 +1,6 @@
+a=int(input("enter a number: "))
+if a/2==0:
+    print("it is even number")
+else:
+    print("it is odd number")
+    print("thank you")
---
+++ sumintegers.py
@@ -0,0 +1,5 @@
+s = 0
+for number in range(1,51):
+    s=s+number
+    print(s)
+
